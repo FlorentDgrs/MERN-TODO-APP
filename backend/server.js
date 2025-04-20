a51d24624d3ed3c16982c044d764201b186d38ef
Florent <florent.degeorges@gmail.com>
2025-04-20 14:06:30 +0200

    update server js

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
-import path from "path";
 import cors from "cors";
-
+import path from "path";
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -12,14 +11,14 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-//app.use(cors())
+// app.use(cors());
 
 app.use("/api/todos", todoRoutes);
 
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(Path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
@@ -27,5 +26,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   connectDB();
-  console.log("server started at http://localhost:5000");
+  console.log("Server started at http://localhost:5000");
 });
